48f4e7bf9122e7558fe5d383c4ea58af2cbbb4d0
nanom80 <nanomegi@gmail.com>
2025-04-28 10:23:19 +0900

    2025-04-28 10:23

---
+++ src/com/javaex/ex05/Ex08.java
@@ -0,0 +1,34 @@
+package com.javaex.ex05;
+
+public class Ex08 {
+
+	public static void main(String[] args) {
+		
+		int[] arrA = new int[5];
+		
+		arrA[0] = 3;
+		arrA[1] = 6;
+		arrA[2] = 7;
+		
+
+		int[] arrB = new int[5];
+		
+		arrB[0] = 3;
+		arrB[1] = 6;
+		arrB[2] = 7;
+		
+		arrA[1] = 100;
+		
+		for(int i=0; i<arrA.length; i++) {
+			System.out.println(arrA[i]);
+		}
+		
+		System.out.println("---------------");
+		
+		for(int i=0; i<arrB.length; i++) {
+			System.out.println(arrB[i]);
+		}
+		
+	}
+
+}
